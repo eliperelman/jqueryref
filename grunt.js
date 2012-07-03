6409ffbf8126feb503f2702e8f3dbeb96cbbe770
Eli Perelman <eli@eliperelman.com>
2012-07-02 19:01:18 -0500

    Updating .gitignore

--- grunt.js
+++ grunt.js
@@ -1,11 +1,25 @@
 var gfm = require('github-flavored-markdown').parse;
+var gift = require('gift');
+
 var repoTarget = 'jquery/jquery';
+var repo = gift('.');
 
 
 module.exports = function (grunt) {
 
 
-	grunt.registerTask('clean', function () {
+	grunt.registerTask('test', function () {
+		grunt.file.recurse('./css', function (abspath, rootdir, subdir, filename) {
+			grunt.file.copy(abspath, './site/' + filename);
+		});
 
+		//grunt.file.copy('./css/**/*', './site/css');
+
+		repo.checkout('gh-pages', function () {
+			//grunt.file.copy('./site/**/*', '.');
+			grunt.file.recurse('./site', function (abspath, rootdir, subdir, filename) {
+				grunt.file.copy(abspath, './' + filename);
+			});
+		});
 	});
 };
