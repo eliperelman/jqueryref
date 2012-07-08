23940d487283717dbf214388c14f85ec2c5a5e08
Eli Perelman <eli@eliperelman.com>
2012-07-07 22:05:15 -0500

    Deploying source to GitHub Pages

--- assets/js/core.js
+++ assets/js/core.js
@@ -3,12 +3,18 @@
 	$(function () {
 		$('pre code[class*="lang-"]').each(function () {
 			var el = $(this);
+			var content = el.html();
+			var lang = _.reduce( el.attr('class').split(' '), function (acc, value) {
+				if (value.indexOf('lang-') === -1) {
+					return acc;
+				}
+
+				return value.substring(5);
+			});
 
 			el.closest('pre').addClass('cm-s-ambiance');
-			// var lang = el.attr('lang');
-			// var content = el.html();
 
-			// CodeMirror.runMode(content, { name: lang }, this);
+			CodeMirror.runMode(content, { name: lang }, this);
 		});
 	});
 
