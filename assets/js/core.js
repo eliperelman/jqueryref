071ec8fdf30be580ff4abfad82e3c3b6ead7cb0e
Eli Perelman <eli@eliperelman.com>
2012-07-07 20:40:10 -0500

    Deploying source to GitHub Pages

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,39 +1,13 @@
 (function () {
-	var mimes = {
-		html: 'text/html',
-		css: 'text/css',
-		javascript: 'text/javascript'
-	};
-
-
-
 
 	$(function () {
 		$('.highlight [lang]').each(function () {
-
-
 			var el = $(this);
 			var lang = el.attr('lang');
-			var code = el.html();
+			var content = el.html();
 
-			el.empty();
-
-			CodeMirror(this, {
-				value: code,
-				mode: lang,
-				lineNumbers: false,
-				readOnly: true
-			});
-
-
-
-			// var el = $(this);
-			
-			// var content = el.text();
-
-			// CodeMirror.runMode(content, mimes[lang], el[0]);
+			CodeMirror.runMode(content, mimes[lang], this);
 		});
 	});
-
 
 })();
