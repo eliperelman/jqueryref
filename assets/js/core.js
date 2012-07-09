fbdf98c1e0833ed1444112ab626696c6b50453b1
Eli Perelman <eli@eliperelman.com>
2012-07-09 10:54:20 -0500

    Deploying source to GitHub Pages

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,9 +1,9 @@
 (function () {
 	var syntaxHighlight = function () {
-		$('pre code[class*="lang-"]:not(.highlighted)').each(function () {
+		$('pre code:not(.highlighted)').each(function () {
 			var el = $(this).addClass('highlighted');
 			var content = el.html();
-			var lang = 'javascript';
+			var lang = el.attr('class');
 
 			el.closest('pre').addClass('cm-s-ambiance');
 
